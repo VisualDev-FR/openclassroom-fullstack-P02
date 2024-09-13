eaba38c572bd9f4fa3bcf0a16a2d5586086f6afa
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2024-09-13 10:45:56 +0200

    setup data injection from json file

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -2,30 +2,34 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
+import { OlympicCountry } from 'src/app/core/models/Olympic';
+
+
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class OlympicService {
-  private olympicUrl = './assets/mock/olympic.json';
-  private olympics$ = new BehaviorSubject<any>(undefined);
 
-  constructor(private http: HttpClient) {}
+    private olympicUrl = './assets/mock/olympic.json';
+    private olympics$ = new BehaviorSubject<OlympicCountry[] | undefined>(undefined);
 
-  loadInitialData() {
-    return this.http.get<any>(this.olympicUrl).pipe(
-      tap((value) => this.olympics$.next(value)),
-      catchError((error, caught) => {
-        // TODO: improve error handling
-        console.error(error);
-        // can be useful to end loading state and let the user know something went wrong
-        this.olympics$.next(null);
-        return caught;
-      })
-    );
-  }
+    constructor(private http: HttpClient) { }
 
-  getOlympics() {
-    return this.olympics$.asObservable();
-  }
+    loadInitialData() {
+        return this.http.get<any>(this.olympicUrl).pipe(
+            tap((value) => this.olympics$.next(value)),
+            catchError((error, caught) => {
+                // TODO: improve error handling
+                console.error(error);
+                // can be useful to end loading state and let the user know something went wrong
+                this.olympics$.next(undefined);
+                return caught;
+            })
+        );
+    }
+
+    getOlympics() {
+        return this.olympics$.asObservable();
+    }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
+import { OlympicCountry } from 'src/app/core/models/Olympic';
+
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public olympics$: Observable<any> = of(null);
 
-  constructor(private olympicService: OlympicService) {}
+    public olympics$!: Observable<OlympicCountry[] | undefined>; // = of(null) -> ?
 
-  ngOnInit(): void {
-    this.olympics$ = this.olympicService.getOlympics();
-  }
+    constructor(private olympicService: OlympicService) { }
+
+    ngOnInit(): void {
+        this.olympics$ = this.olympicService.getOlympics();
+    }
 }
